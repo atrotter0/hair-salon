0178f4ba4301f7f6211c564b4c1efe9192f28a7d
Abel Trotter <a_trotter0@hotmail.com>
2018-07-13 14:44:10 -0700

    Add bootstrap 4, add stylists controller, add index view, add index tests

---
+++ HairSalon.Tests/ControllerTests/StylistsControllerTest.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class StylistsControllerTest : IDisposable
+    {
+        public void Dispose()
+        {
+            Stylist.DeleteAll();
+        }
+
+        public StylistsControllerTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=abel_trotter_test;";
+        }
+
+        [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            StylistsController controller = new StylistsController();
+            ActionResult Index = controller.Index();
+            Assert.IsInstanceOfType(Index, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Index_HasCorrectModelType_Account()
+        {
+            ViewResult Index = new StylistsController().Index() as ViewResult;
+            var result = Index.ViewData.Model;
+            Assert.IsInstanceOfType(result, typeof(List<Stylist>));
+        }
+    }
+}
