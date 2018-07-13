fb880c596c9b0602ae7e562e9123f00205eaea32
Abel Trotter <a_trotter0@hotmail.com>
2018-07-13 16:19:39 -0700

    Add show, delete, edit, update routes for stylist controller

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -25,7 +25,38 @@ namespace HairSalon.Controllers
         {
             Stylist stylist = new Stylist(name);
             stylist.Save();
-            return View("Index");
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("/stylists/{id}")]
+        public ActionResult Show(int id)
+        {
+            Stylist stylist = Stylist.Find(id);
+            return View(stylist);
+        }
+
+        [HttpGet("/stylists/{id}/edit")]
+        public ActionResult Edit(int id)
+        {
+            Stylist stylist = Stylist.Find(id);
+            return View(stylist);
+        }
+
+        [HttpPost("/stylists/{id}/update")]
+        public ActionResult Update(int id, string name)
+        {
+            Stylist stylist = Stylist.Find(id);
+            stylist.Name = name;
+            stylist.Update();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("/stylists/{id}/delete")]
+        public ActionResult Delete(int id)
+        {
+            Stylist stylist = Stylist.Find(id);
+            stylist.Delete();
+            return RedirectToAction("Index");
         }
     }
 }
